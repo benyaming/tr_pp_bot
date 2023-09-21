465a4f59d4cc94267ee0ba050caed2eac6a926b2
Benyamin Ginzburg <benyomin.94@gmail.com>
2023-09-21 08:39:05 +0300

    Remove test commands

--- tr_pp_bot/apps/tr_main/commands/__init__.py
+++ tr_pp_bot/apps/tr_main/commands/__init__.py
@@ -1,9 +1,8 @@
 from typing import Type
 
 from .job import JobCommand
-from .job_p import JobCommand as J2
 from ...base_command import BaseCommand
 
 
 def get_commands() -> list[Type[BaseCommand]]:
-    return [JobCommand, J2]
+    return []
